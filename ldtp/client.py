5f6b6d225863f6a8d078328ceaed4c271ace7383
Florian Schüller <florian.schueller@gmail.com>
2017-06-04 14:52:20 +0200

    simple retry - to narrow down those ldtp problems

--- ldtp/client.py
+++ ldtp/client.py
@@ -72,7 +72,11 @@ class _Method(xmlrpclib._Method):
             logger.debug('%s(%s)' % (self.__name, \
                                          ', '.join(map(repr, args) + ['%s=%s' % (k, repr(v)) \
                                                                           for k, v in kwargs.items()])))
-        return self.__send(self.__name, args)
+        try:
+            return self.__send(self.__name, args)
+        catch BaseException as be:
+            logger.debug('%s(%s) Trying once more' % (self.__name, str(be)))
+        return return self.__send(self.__name, args)
 
 class Transport(xmlrpclib.Transport):
     def _handle_signal(self, signum, frame):
